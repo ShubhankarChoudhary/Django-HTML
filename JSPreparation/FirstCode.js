203137056a1f9ea370adce46db2257a5d1e061c4
ShubhankarChoudhary <shubhankar.choudhary782@gmail.com>
2020-06-11 00:33:11 +0530

    Starting of Javascript

---
+++ JSPreparation/FirstCode.js
@@ -0,0 +1,17 @@
+var fName='Wolverine';
+var age=25;
+var alive=true;
+var salary;
+console.log(salary);
+salary=50000;
+console.log(salary);
+console.log(alive);
+console.log(fName);
+console.log(age);
+console.log('Hello World!!!!!');
+var company
+/*company='ABC';*/
+console.log(company);
+alert('Hi! How are you?');
+var company=prompt('Which company do you work for?');
+console.log(company);
