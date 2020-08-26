7641c69ebafe4dcb8bca724779e093a5dd553d0e
ShubhankarChoudhary <shubhankar.choudhary782@gmail.com>
2020-08-26 17:13:23 +0530

    JS Started

---
+++ JSPreparation/support1.js
@@ -0,0 +1,42 @@
+/*
+var fName='Shubh';
+var place='Deoghar';
+var age=25;
+var age='Twenty five';
+var isMale=true;
+var salary;
+console.log(fName)
+console.log(age)
+console.log(place)
+console.log(isMale)
+console.log(salary)
+salary='Aim High'
+console.log('Hello World!!!!!')
+console.log(salary)
+*/
+// console.log('Hello World!!!!')
+
+//Variable Mutation and Type Coersion
+
+var Name='Shubh'
+var age=25
+
+console.log(Name+' '+age)
+
+var sName, isMarried, s_age, Profession;
+
+console.log(sName+' '+isMarried+' '+s_age+' '+Profession)
+
+sName='Alpha'
+isMarried= false
+s_age=25
+Profession='Student'
+
+console.log(sName+' '+isMarried+' '+s_age+' '+Profession)
+
+alert(sName+' '+isMarried+' '+s_age+' '+Profession)
+
+var input=prompt('What is your name ?');
+
+console.log(input)
+
