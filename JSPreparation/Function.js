b7d2f1a7abc99284b47e072700fad56a0e4cb7d4
ShubhankarChoudhary <shubhankar.choudhary782@gmail.com>
2020-09-14 13:32:02 +0530

    Sticky Notes App Setup

--- JSPreparation/Function.js
+++ JSPreparation/Function.js
@@ -112,6 +112,306 @@ console.log(NewBill)
 // Key Value Pair
 
 
+var John = {
+    fName : 'John',
+    lName : 'Franklin',
+    BirthYear : 1989,
+    isMarried : false ,
+    Job : 'IT Professional',
+    FamilyMembers : ['Alpha','Betta','Gamma']
+}
+
+console.log(John.fName)
+console.log(John.lName)
+console.log(John.isMarried)
+
+John.isMarried = true
+
+console.log(John.isMarried)
+
+console.log(John)
+
+
+// OBJECT LITERAL
+
+
+var Dani = new Object();
+ Dani.fName = 'Daniel'
+ Dani.lName='Cool'
+ Dani.BirthYear = 1988
+ Dani['Job'] = 'IT Professional'
+
+ console.log(Dani)
+
+var Jharkhand = {
+    State : 'Jharkhand',
+    Capital : 'Ranchi',
+    Districts : ['Deoghar','Dumka','Dhanbad'],
+    NoOfTouristSpots : 5,
+    IncomeOfState : function(NoOfTouristSpots){
+        this.Profit = (10*this.NoOfTouristSpots);
+    }
+}
+console.log(Jharkhand.IncomeOfState())
+// Jharkhand.Profit = Jharkhand.IncomeOfState()
+console.log(Jharkhand)
+
+// BMI of JOHN and MARK
+
+var John = {
+    FullName : 'John Martin',
+    Mass : 75,
+    Height : 1.80,
+    BMI : function(){
+        this.JohnBMI = this.Mass/(this.Height*this.Height)
+        return this.JohnBMI
+    }
+}
+var Mark = new Object();
+Mark = {
+    FullName : 'Mark Luther',
+    Mass : 80,
+    Height : 1.90,
+    BMI : function(){
+        this.MarkBMI = this.Mass/(this.Height*this.Height)
+        return this.MarkBMI
+    }
+}
+
+console.log(John.BMI())
+console.log(John)
+console.log(Mark.BMI())
+console.log(Mark)
+
+if (John.BMI()>Mark.BMI()){
+    console.log(John.FullName+'  '+John.JohnBMI)
+}
+else if(John.JohnBMI<Mark.MarkBMI){
+    console.log(Mark.FullName+'  '+Mark.MarkBMI)
+}
+else{
+    console.log('Same BMI value for both')
+}
+
+
+// Loops
+
+
+for (var i=0; i<=9;i++){
+    console.log(i)
+}
+
+for (i=10; i>=1;i--){
+    console.log(i)
+}
+
+
+FamilyMembers = ['Mom','Dad',22,'Ansh','Harsh',25]
+// for (i=0;i<FamilyMembers.length;i++){
+//     console.log(FamilyMembers[i])
+// }
+// i=0
+// while(i<FamilyMembers.length){
+//     console.log(FamilyMembers[i])
+//     i++
+// }
+
+for (i=0;i<10;i++){
+    if (typeof FamilyMembers[i] === 'string'){
+        continue;
+    }
+    console.log(FamilyMembers[i])
+}
+
+// Challenge
+var John = {
+    Bill : [124,48,268,180,42],
+    tip : [],
+    Total : [],
+    calTip : function(Bill){
+        console.log("***********John's Details Starts**************")
+        for (i=0;i<this.Bill.length;i++){
+            if (this.Bill[i]<50){
+                this.tip[i]=0.2*this.Bill[i]
+                this.Total[i]=CalculateTotal(this.Bill[i],this.tip[i])                
+            }
+            else if (this.Bill[i]>=50 || this.Bill[i]<=200){
+                this.tip[i]=0.15*this.Bill[i]
+                this.Total[i]=CalculateTotal(this.Bill[i],this.tip[i])
+            }
+            else if (this.Bill[i]>200){
+                this.tip[i]=0.1*this.Bill[i]
+                this.Total[i]=CalculateTotal(this.Bill[i],this.tip[i])
+            }
+        }
+        console.log('Bill of 5 Restaurants : - '+this.Bill)
+        console.log('Tip for the Bills : - '+this.tip)
+        console.log('Total Amount : - '+this.Total)
+        tiptotal=0
+        for (i=0;i<this.Bill.length;i++){
+            tiptotal=tiptotal+this.tip[i]
+        }
+        tipavgJ=tiptotal/this.Bill.length
+        console.log(tipavgJ+' is the average of Tip given by John')
+        console.log("***********John's Details Ends**************")
+    }
+}
+
+
+var Mark = {
+    Bill : [77,375,110,45],
+    tip : [],
+    Total : [],
+    calTip : function(Bill){
+        console.log("***********Mark's Details Starts**************")
+        for (i=0;i<this.Bill.length;i++){
+            if (this.Bill[i]<100){
+
+                this.tip[i]=0.2*this.Bill[i]
+                this.Total[i]=CalculateTotal(this.Bill[i],this.tip[i])                
+            }
+            else if (this.Bill[i]>=100 && this.Bill[i]<=300){
+                console.log(this.Bill[i])
+                this.tip[i]=0.10*this.Bill[i]
+                this.Total[i]=CalculateTotal(this.Bill[i],this.tip[i])
+            }
+            else if (this.Bill[i]>300){
+                this.tip[i]=0.25*this.Bill[i]
+                this.Total[i]=CalculateTotal(this.Bill[i],this.tip[i])
+            }
+        }
+        console.log('Bill of 5 Restaurants : - '+this.Bill)
+        console.log('Tip for the Bills : - '+this.tip)
+        console.log('Total Amount : - '+this.Total)
+        tiptotal=0
+        for (i=0;i<this.Bill.length;i++){
+            tiptotal=tiptotal+this.tip[i]
+        }
+        tipavgM=0
+        tipavgM=tiptotal/this.Bill.length
+        console.log(tipavgM+' is the average of Tip given by Mark')
+        console.log(tiptotal)
+        console.log("***********Mark's Details Starts**************")
+        
+    }
+}
+
+John.calTip()
+Mark.calTip()
+
+function CalculateTotal(Bills,Tips){
+    var Totals;
+    Totals=Tips+Bills
+    return Totals
+}
+if (tipavgJ>tipavgM){
+    console.log('John pays more tip')
+}
+else console.log('Mark pays more tip')
+
+
+
+var classes = 'Tenth';
+var classes = 'Ninety ninety eight'
+
+if (classes===window.classes){
+    console.log('True')
+}
+else console.log('False')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ sticky-notes/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h1 style={{color:"blue",margin:0}}>React JS </h1>
+    </div>
+  );
+}
+
+export default App;
